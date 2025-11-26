0340ba1ab8287ab825329f28159be1d9ba28c234
Seliudev <ulucamselinn@gmail.com>
2025-11-26 15:35:50 +0300

    Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+// src/App.tsx
+const App = () => (
+    <div className="min-h-screen bg-gray-100 flex justify-center items-center">
+        <h1 className="text-4xl font-bold text-blue-500"></h1>
+    </div>
+)
+
+export default App;
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,16 @@
+
+const NavBar = () => {
+    return (
+        <>
+        <div className="flex space-between">
+            <div>Please blah blah blah!</div>
+            <div>
+                <div>Log in</div>
+                <div>Shopping Cart</div>
+                <div>Search bar</div>
+            </div>
+        </div>
+        </>
+    );
+};
+export default NavBar;
